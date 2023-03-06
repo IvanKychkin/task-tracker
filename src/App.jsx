2ff3fc65ad045c451f56e6ce34b22f107673b7e6
Ivan Kychkin <ivan.kychkin@stackers.team>
2023-03-06 15:13:53 +0300

    Installing eslint, prettier, husky

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css';
+import GlobalStyle from "./globalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <div>TASK-TRACKER APP!</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
